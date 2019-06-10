7af62a5d72c1d75816329a651eb7bc4a09d0feb8
kiotaku <takumi.kioka@gmail.com>
2019-06-10 16:11:09 +0900

    fix bug & typo

--- cats-api/index.js
+++ cats-api/index.js
@@ -3,13 +3,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 mongoose.Promise = Promise
 const app = express()
 
-const Cat = new Schema({
+const CatSchema = new Schema({
   name: String,
   dateOfBirth: { type: Date, max: Date.now() },
   gender: { type: String, enum: ['male', 'female'] },
@@ -17,13 +18,31 @@ const Cat = new Schema({
   weight: Number
 })
 
-mongoose.connection(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
+const Cat = mongoose.model('Cat', CatSchema)
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
   console.log('Connected successfully.')
   app.listen(process.env.PORT)
 }).catch(err => {
   console.log('Connection to db failed: ' + err)
 })
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/cats', (req, res) => {
+  const { name, dateOfBirth, gender, color, weight } = req.body
+  Cat.create({
+    name,
+    dateOfBirth: new Date(dateOfBirth).getTime(),
+    gender,
+    color,
+    weight
+  }).then(cat => {
+    res.send(cat)
+  })
 })
