474bd5f46b3eebf4f25c96e83048a31abb746d12
kiotaku <takumi.kioka@gmail.com>
2019-06-10 15:14:06 +0900

    Add Mongoose in cats api

--- cats-api/index.js
+++ cats-api/index.js
@@ -1,8 +1,21 @@
+'use strict'
+
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.Promise = Promise
 const app = express()
+
+mongoose.connection(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
+  console.log('Connected successfully.')
+  app.listen(process.env.PORT)
+}).catch(err => {
+  console.log('Connection to db failed: ' + err)
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.listen(3000)
